9231ae418b00fa6cf4b6c3ccc6554f7935e1d264
yash <yashgudhka98@gmail.com>
2023-10-18 14:26:39 +0530

    fastfood CRUD added

---
+++ app/Http/Controllers/FastFoodController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\FastFood;
+use Illuminate\Http\Request;
+
+class FastFoodController extends Controller
+{
+    public function index()
+    {
+        $fastfoods = FastFood::all();
+        return view('fastfood.index', compact('fastfoods'));
+    }
+
+    public function create()
+    {
+        return view('fastfood.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'=>'required'
+        ]);
+        $data=new FastFood();
+        $data->name=$request->name;
+        $data->save();
+        return redirect()->route('fastfood.index')->with('success','FastFood Added Successfully');
+    }
+
+    public function edit($id)
+    {
+        $data=FastFood::find($id);
+        return view('fastfood.edit', compact('data'));
+    }
+
+    public function update(Request $request)
+    {
+        $fastfood = FastFood::find($request->id);
+        $fastfood->name = $request->name;
+        $fastfood->save();
+        return redirect()->route('fastfood.index')->with('success','FastFood Updated Successfully');
+    }
+
+    public function destroy($id)
+    {
+        $data=FastFood::find($id);
+        $data->delete();
+        return redirect()->route('fastfood.index')->with('success','FastFood Deleted Successfully');
+    }
+}
---
+++ app/Models/FastFoodSubCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FastFoodSubCategory extends Model
+{
+    use HasFactory;
+    protected $table = 'fastfood_sub_category';
+}
